45538cf7f8965486463d09730c227b27784f0584
putritammara <tammaraputri@student.telkomuniversity.ac.id>
2022-04-18 22:26:32 +0700

    [Done: Admin bisa logout resolve #12]

--- LANJALAN/app/Http/Controllers/AuthController.php
+++ LANJALAN/app/Http/Controllers/AuthController.php
@@ -58,6 +58,6 @@ class AuthController extends Controller
     {
        $request->session()->flush();
        Auth::logout();
-       return Redirect('login');
+       return Redirect('/');
     }
 }
