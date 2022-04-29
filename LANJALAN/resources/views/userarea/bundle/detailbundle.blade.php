6d12c5f2d7ceff11fda87e5814436c0316fade57
Nazharr <nabeel.agarnida@gmail.com>
2022-04-29 21:56:41 +0700

    [Add: User bisa melihat detail paket bundling ref #25]

---
+++ LANJALAN/resources/views/userarea/bundle/detailbundle.blade.php
@@ -0,0 +1,32 @@
+@extends('layoutsmain.main')
+
+@section('container')
+    <div class="p-5 m-5">
+    <!-- show Detail Wisata -->
+            <div class="mb-3">
+                <h3 class="text-center">{{ $detailbundle->judulBundle }}</h3>
+            </div>
+
+            <div class="text-center m-5">
+            <img class= "w-50" src="/img/papuma.jpg" alt=" ">
+            </div>
+            <hr>
+            <form class="me-auto">
+                <div class="mb-3">
+                    <label  class="form-label fw-bold">Harga :</label>
+                    <p>Rp{{ $detailbundle->hargaBundle }}</p>
+                </div>
+                <div class="mb-3">
+                    <label  class="form-label fw-bold">Deskripsi :</label>
+                    <p>{{ $detailbundle->deskripsiBundle }}</p>
+                </div>
+                <div class="mb-3">
+                    <label  class="form-label fw-bold">Tanggal Expired :</label>
+                    <p>{{ $detailbundle->tanggalExpBundle }}</p>
+                </div>
+                <div class="d-grid">
+                    <button type="submit" class="btn btn-success ">Pesan Sekarang - Rp{{ $detailbundle->hargaBundle }}</button>
+                </div>
+            </form>
+    </div>   
+@endsection
