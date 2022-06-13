0cf4344ab6ed352176a25dcbd0342b8b765b1365
Nayaaa <inayahdwi07@gmail.com>
2022-06-13 21:25:51 +0700

    [Done: Admin Lihat Daftar Pesanan ref #33]

--- LANJALAN/resources/views/dashboard/dash.blade.php
+++ LANJALAN/resources/views/dashboard/dash.blade.php
@@ -59,25 +59,7 @@
             @if( $p->buktiTf == "Belum Ada Foto")
             <small class="text-danger" >{{ $p->buktiTf }}</small>
             @else
-            <a data-bs-toggle="modal" data-bs-target="#exampleModal"><img class="w-50" src="img/images/{{ $p->buktiTf }}"></a>
-
-            <!-- Modal -->
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-xl">
-                <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Bukti Pembayaran</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <img class="w-100" src="img/images/{{ $p->buktiTf }}">
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                </div>
-                </div>
-            </div>
-            </div>
+             <br>
             @endif
         </td>
         <td>
